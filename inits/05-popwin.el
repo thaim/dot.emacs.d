267fdebd85c371a9f2a55df71f11c0093cd382df
thaim <thaim24@gmail.com>
2017-01-16 23:40:21 +0900

    add config for popwin

--- inits/05-popwin.el
+++ inits/05-popwin.el
@@ -1,1 +1,13 @@
 (el-get-bundle! popwin)
+
+(setq display-buffer-function 'popwin:display-buffer)
+; show popup window in bottom
+(setq popwin:popup-window-position 'bottom)
+
+; for auto-async-byte-compile
+(push '(" *auto-async-byte-compile*" :position bottom) popwin:special-display-config)
+
+;; for yatex-mode
+;; does not work with load yatex mode
+;; (require 'popwin-yatex)
+;; (push '("*YaTex-typesetting*") popwin:special-display-config)
