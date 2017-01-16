f615dd6e1d1a2d70193265a110e56ebf6cfa10ef
thaim <thaim24@gmail.com>
2017-01-16 23:06:10 +0900

    load auto-complete in initialize

--- inits/10-auto-complete.el
+++ inits/10-auto-complete.el
@@ -1,4 +1,4 @@
 ;;;; auto-complete
-(el-get-bundle auto-complete
+(el-get-bundle! auto-complete
   ;; completion history file path
   (setq ac-comphist-file (expand-file-name "cache/ac-comphist.dat" user-emacs-directory)))
