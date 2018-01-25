7d2f6fd003f7cc2a2774e690345e9a6db25514fe
thaim <thaim24@gmail.com>
2018-01-25 21:31:52 +0900

    Configure yasnippet mode

--- inits/11-yasnippet.el
+++ inits/11-yasnippet.el
@@ -1,3 +1,16 @@
-;;yasnippet
-;;(require 'yasnippet-bundle)
-;(bundle yasnippet-bundle)
+;;; yasnippet
+(el-get-bundle! yasnippet)
+
+(add-to-list 'yas-snippet-dirs (locate-user-emacs-file "etc/yasnippets"))
+(add-to-list 'yas-snippet-dirs (locate-user-emacs-file "etc/yasnippet-snippets/snippets"))
+
+
+(custom-set-variables '(yas-trigger-key "TAB"))
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+
+(yas-global-mode 1)
