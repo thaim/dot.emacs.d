b88ee6d4cce27429d6fc66711372c8aaf98f6c09
thaim <thaim24@gmail.com>
2019-02-26 05:50:49 +0900

    use indent: 2 space for javascript

--- inits/11-javascript.el
+++ inits/11-javascript.el
@@ -4,3 +4,7 @@
 ;(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.json\\'" . jcript-mode))
 ;;(autoload 'javascript-mode "javascript" nil t)
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
