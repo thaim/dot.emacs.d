ac1bb419b03ee84446862741919833febc778e1d
thaim <thaim24@gmail.com>
2019-02-27 05:39:19 +0900

    auto-compile が度々失敗するので？無効化する

--- inits/30-auto-compile.el
+++ inits/30-auto-compile.el
@@ -9,5 +9,5 @@
 
 ;;;auto-async-byte-compile
 ;;;elispを自動でバイトコンパイル
-(el-get-bundle auto-async-byte-compile)
-(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+; (el-get-bundle auto-async-byte-compile)
+; (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
