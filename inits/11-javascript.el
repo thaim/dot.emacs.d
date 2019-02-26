7d851cf6e6a87c34b30cfebacef440104fb1b87b
thaim <thaim24@gmail.com>
2019-02-27 05:40:20 +0900

    vue-modeがjs-modeを利用するようなので、javascriptにはjs-modeで統一する

--- inits/11-javascript.el
+++ inits/11-javascript.el
@@ -1,10 +1,31 @@
 ;;;java-script
-(el-get-bundle! js2-mode)
+;; EmacsにおけるJavaScriptは
+;; * Emacs 23.2から標準で梱包されるようになった js-mode
+;; * 従来人気の高かった js2-mode
+;; が選択肢。ここではjs-modeを利用する
 
-;(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-;;(add-to-list 'auto-mode-alist '("\\.json\\'" . jcript-mode))
-;;(autoload 'javascript-mode "javascript" nil t)
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (make-local-variable 'js-indent-level)
-            (setq js-indent-level 2)))
+
+;; 書籍: Emacs実践入門に記載のjs-modeインデント設定
+(defun js-indent-hook ()
+  ;; インデント幅を4にする
+  (setq js-indent-level 2
+        js-expr-indent-offset 2
+        indent-tabs-mode nil)
+  ;; switch文のcaseラベルをインデントする関数を定義する
+  (defun my-js-indent-line ()
+         (interactive)
+         (let* ((parse-status (save-excursion (syntax-ppss (point-at-bol))))
+                (offset (- (current-column) (current-indentation)))
+                (indentation (js--proper-indentation parse-status)))
+           (back-to-indentation)
+           (if (looking-at "case\\s-")
+               (indent-line-to (+ indentation 2))
+             (js-indent-line))
+           (when (> offset 0) (forward-char offset))))
+  ;; caseラベルのインデント処理をセットする
+  (set (make-local-variable 'indent-line-function) 'my-js-indent-line)
+  ;; ここまでcaseラベルを調整する設定
+  )
+
+;; js-modeの起動時にhookを追加
+(add-hook 'js-mode-hook 'js-indent-hook)
