20169cd9f53fde44406f6a7ee7aa18a3de91d7e6
thaim <thaim24@gmail.com>
2019-03-15 21:05:52 +0000

    fix wrong indent mode for golang: use tab

--- inits/11-golang.el
+++ inits/11-golang.el
@@ -12,7 +12,7 @@
        (add-hook 'before-save-hook' 'gofmt-before-save)
        (local-set-key (kbd "M-.") 'godef-jump)
        (set (make-local-variable 'company-backends) '(company-go))
-       (setq indent-tabs-mode nil)    ; タブを利用
+       (setq indent-tabs-mode t)    ; タブを利用
        (setq c-basic-offset 4)    ; tabサイズを4にする
        (setq tab-width 4)))
 
