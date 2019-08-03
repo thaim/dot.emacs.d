c846dae7d6d870bc7dd7973ed83644aa4696156a
thaim <thaim24@gmail.com>
2019-08-03 18:58:49 +0900

    apply indent highlight for yaml

--- inits/11-yaml.el
+++ inits/11-yaml.el
@@ -1,3 +1,10 @@
 (el-get-bundle! yaml-mode)
 
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+;; インデントをハイライトする
+(el-get-bundle antonj/Highlight-Indentation-for-Emacs)
+(set-face-background 'highlight-indentation-face "#e3e3d3")
+(set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+
+(add-hook 'yaml-mode-hook 'highlight-indentation-mode)
