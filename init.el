9a885b448b824bfe960a8a8dc2600c239241702b
thaim <thaim24@gmail.com>
2019-11-10 02:29:31 +0900

    update package-archive url to use https

--- init.el
+++ init.el
@@ -5,9 +5,9 @@
                 package-user-dir (expand-file-name "elpa" versioned-dir)))
 
 (setq package-archive
-      '(("gnu"   . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")
-        ("org"   . "http://orgmode.org/elpa/")))
+      '(("gnu"   . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")
+        ("org"   . "https://orgmode.org/elpa/")))
 (package-initialize)
 
 ;; proxy configuration
