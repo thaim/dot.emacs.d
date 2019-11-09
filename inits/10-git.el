68e9347f3d2c409bf7f4dc4cde116d30a5b79bcc
thaim <thaim24@gmail.com>
2019-11-10 01:54:02 +0900

    fix magit history file directory into cache dir

--- inits/10-git.el
+++ inits/10-git.el
@@ -1,5 +1,9 @@
-(el-get-bundle! magit)
+(el-get-bundle magit)
 
 (global-set-key (kbd "C-x g") 'magit-status)
+; store history file in cache dir
+(setq transient-history-file
+      (expand-file-name "cache/transient/history.el" user-emacs-directory))
+
 
 (el-get-bundle! git-modes)
