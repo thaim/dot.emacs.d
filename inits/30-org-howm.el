da9f2c60cadf1f8fc92aa6a926ac136d51894537
thaim <thaim24@gmail.com>
2023-02-05 22:54:21 +0900

    apply leaf to org mode

--- inits/30-org-howm.el
+++ inits/30-org-howm.el
@@ -36,7 +36,9 @@
 
 ;;; org-mode
 
-(require 'org)
-(setq org-modules (append org-modules '(org-odt))) ;ODT機能(開発中)を有効化
-(add-hook 'org-mode-hook 'howm)
-(add-to-list 'auto-mode-alist '("\\.howm$" . org-mode))
+(leaf org
+  ensure: t
+  :mode ("\\.howm$")
+  :hook
+  (org-mode-hook . howm-mode)
+  )
