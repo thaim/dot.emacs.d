e6f230fdd960a4a729341ef709029667f7d61ff5
thaim <thaim24@gmail.com>
2023-02-05 22:32:59 +0900

    merge howm and org inits into single file

--- inits/30-org-howm.el
+++ inits/30-org-howm.el
@@ -38,3 +38,13 @@
 ;; (bundle howm
 ;;   (with-eval-after-load-feature 'howm-vars
 ;;        (set-face-background 'howm-view-name-face "DimGray")))
+
+
+;;org-mode
+
+(require 'org)
+(setq org-modules (append org-modules '(org-odt))) ;ODT機能(開発中)を有効化
+(add-hook 'org-mode-hook 'howm)
+(add-to-list 'auto-mode-alist '("\\.howm$" . org-mode))
+;(setq howm-view-title-header "=")
+(setq howm-todo-menu-types "[-+~!]")	;完了済みToDoは非表示
