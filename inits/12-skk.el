b7958a1e54a4ae497cdbd3dd7c62b65ced0558da
thaim <thaim24@gmail.com>
2023-04-09 19:27:41 +0900

    use more skk jisyo

--- inits/12-skk.el
+++ inits/12-skk.el
@@ -1,3 +1,7 @@
+; 辞書ファイルを事前に ~/.emacs.d/etc/skk/ 以下に配置する。
+; 辞書フィアルは http://openlab.ring.gr.jp/skk/wiki/wiki.cgi?page=SKK%BC%AD%BD%F1 から取得する。
+; geo/jinmei/propernoun など
+
 (leaf ddskk
   :ensure t
   :bind (("C-x C-j" . skk-mode)
@@ -10,4 +14,5 @@
            (skk-tut-file . "~/.emacs.d/elisp/ddskk") ;チュートリアルパス
            (skk-large-jisho . "~/.emacs.d/etc/skk/SKK-JISYO.L") ;Large辞書パス
            (skk-jisyo . "~/.emacs.d/cache/skk/.skk-jisyo") ;個人用辞書パス
+           (skk-extra-jisyo-file-list '("~/.emacs.d/etc/skk/SKK-JISYO.[a-z]+")) ;;追加辞書
            ))
