7f4e4d682c462620498e008d5bafac5dbfe4a2c5
thaim <thaim24@gmail.com>
2023-10-26 00:07:33 +0900

    replace for popwin

--- inits/05-popwin.el
+++ inits/05-popwin.el
@@ -1,16 +1,8 @@
-(el-get-bundle! popwin)
-
-(popwin-mode 1)
-(setq pop-up-windows nil)
-(setq anything-samewindow nil)
-
-; show popup window in bottom
-(setq popwin:popup-window-position 'bottom)
-
-; for auto-async-byte-compile
-(push '(" *auto-async-byte-compile*" :position bottom) popwin:special-display-config)
-
-;; for yatex-mode
-;; does not work with load yatex mode
-;; (require 'popwin-yatex)
-;; (push '("*YaTex-typesetting*") popwin:special-display-config)
+(leaf popwin
+  :ensure t
+  :config
+  (setq pop-up-windows nil)
+  (setq anything-samewindow nil)
+  (setq popwin:popup-window-position 'bottom) ;; show popup window in bottom
+  (popwin-mode 1)
+  )
