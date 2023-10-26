75d62453933d7eb524cfe3dfb2c6809ac21b4da0
thaim <thaim24@gmail.com>
2023-10-27 00:25:50 +0900

    replace dockerfile

--- inits/11-dockerfile.el
+++ inits/11-dockerfile.el
@@ -1,5 +1,8 @@
 ;; dockerfile-mode from
 ;; https://github.com/spotify/dockerfile-mode
 
-(el-get-bundle! dockerfile-mode)
-(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+(leaf dockerfile-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+  )
