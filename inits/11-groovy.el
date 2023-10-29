5ec64501dc38be180b9fa745f13e5af799bedff3
thaim <thaim24@gmail.com>
2023-10-29 23:06:37 +0900

    replace el-get for groovy with leaf

--- inits/11-groovy.el
+++ inits/11-groovy.el
@@ -1,5 +1,5 @@
-(el-get-bundle! 'groovy-mode)
-
-(autoload 'groovy-mode "groovy-mode"
-  "Major mode for editing Groovy files" t)
-(add-to-list 'auto-mode-alist '("\\Jenkinsfile$" . groovy-mode))
+(leaf groovy-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\Jenkinsfile$" . groovy-mode))
+  )
