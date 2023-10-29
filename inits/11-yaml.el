49cefb0fe1e81d458475c2614950d0923116aed6
thaim <thaim24@gmail.com>
2023-10-29 17:51:47 +0900

    replace yaml-mode

--- inits/11-yaml.el
+++ inits/11-yaml.el
@@ -1,11 +1,16 @@
-(el-get-bundle! yaml-mode)
+(leaf yaml-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+  (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
-
-;; インデントをハイライトする
-(el-get-bundle! highlight-indentation)
-(set-face-background 'highlight-indentation-face "#e3e3d3")
-(set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
-
-(add-hook 'yaml-mode-hook 'highlight-indentation-mode)
+  ;; インデントをハイライトする
+  (leaf highlight-indentation
+    :ensure t
+    :config
+    (set-face-background 'highlight-indentation-face "#e3e3d3")
+    (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+    :hook
+    (yaml-mode-hook . highlight-indentation-mode)
+    )
+  )
