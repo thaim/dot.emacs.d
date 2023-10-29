574cb33e5cc2d951d60cca5551854115e4a266d0
thaim <thaim24@gmail.com>
2023-10-29 18:25:30 +0900

    replace elscreen
    elscreen-persist not working

--- inits/20-elscreen.el
+++ inits/20-elscreen.el
@@ -2,25 +2,24 @@
 ;;  elscreen                                              ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(el-get-bundle! elscreen)
+(leaf elscreen
+  :ensure t
+  :config
+  (setq elscreen-prefix-key "\C-o")           ;; elscreenのprefixキー
+  (setq elscreen-tab-display-kill-screen nil) ;; タブの左端の閉じるボタンを表示しない
+  (elscreen-start)
+  (define-key elscreen-map (kbd "C-h") 'elscreen-previous)
+  (define-key elscreen-map (kbd "C-l") 'elscreen-next)
 
-(setq elscreen-prefix-key "\C-o")       ;elscreenのprefixキー
-(define-key elscreen-map "h" 'elscreen-previous) ;各種ショートカット
-(define-key elscreen-map "\C-h" 'elscreen-previous)
-(define-key elscreen-map [C-A-right] 'elscreen-previous)
-(define-key elscreen-map "l" 'elscreen-next)
-(define-key elscreen-map "\C-l" 'elscreen-next)
-(define-key elscreen-map [C-A-left] 'elscreen-next)
-(setq elscreen-tab-display-kill-screen nil) ;タブの左端の閉じるボタンを表示しない
-(elscreen-start)
+  ;;; elscreenのタブをemacs再起動後も継続する
+  (leaf elscreen-persist
+    :ensure t
+    :config
+    (setq elscreen-persist-file (expand-file-name "cache/elscreen" user-emacs-directory))
+    (elscreen-persist-mode 1)
+    )
+  )
 
-
-;;; elscreen-persist
-(el-get-bundle! elscreen-persist)
-(elscreen-persist-mode 1)
-
-;; elscreen configuration file locate in cache directory
-(setq elscreen-persist-file (expand-file-name "cache/elscreen" user-emacs-directory))
 
 ;; (require 'elscreen-plus nil t)		;screenの一覧をエコー領域に表示
 ;; (require elscreen-howm)		;elscreen-howmの導入(howmのページを新しいscreenで開く)
