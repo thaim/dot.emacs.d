dae1542f12854f47233f26eee33cbfd73521932b
thaim <thaim24@gmail.com>
2023-11-01 23:00:59 +0900

    merge yaml into programming

--- inits/11-programming.el
+++ inits/11-programming.el
@@ -163,3 +163,23 @@
   :hook
   (ansible-mode-hook . lsp)
   )
+
+;;;;
+;;;; for yaml
+;;;;
+(leaf yaml-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+  (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+  ;; インデントをハイライトする
+  (leaf highlight-indentation
+    :ensure t
+    :config
+    (set-face-background 'highlight-indentation-face "#e3e3d3")
+    (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+    :hook
+    (yaml-mode-hook . highlight-indentation-mode)
+    )
+  )
