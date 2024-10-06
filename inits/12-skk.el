c6307f581cd442820e386f0650718c8b6307c0ff
thaim <thaim24@gmail.com>
2024-10-06 22:23:31 +0900

    remove skk tutorial and change format

--- inits/12-skk.el
+++ inits/12-skk.el
@@ -4,16 +4,19 @@
 
 (leaf ddskk
   :ensure t
-  :bind (("C-x C-j" . skk-mode)
-         ("C-x j" . skk-mode)
-         ("C-x t" . 'skk-tutorial)
-         )
-  :custom (
-           (default-input-method "japanese-skk")
-           (skk-kutouten-type . 'jp) ;句読点には「、。」を利用する (デフォルト挙動だが明記する)
-           (skk-tut-file . "~/.emacs.d/elisp/ddskk") ;チュートリアルパス
-           (skk-large-jisho . "~/.emacs.d/etc/skk/SKK-JISYO.L") ;Large辞書パス
-           (skk-jisyo . "~/.emacs.d/cache/skk/.skk-jisyo") ;個人用辞書パス
-           (skk-extra-jisyo-file-list . (mapcar (lambda (f) (concat "~/.emacs.d/etc/skk/" f))
-                                                (directory-files "~/.emacs.d/etc/skk/" nil "SKK-JISYO\\.[a-z]+"))) ;;追加辞書
-           ))
+  :bind
+  ("C-x C-j" . skk-mode)
+  ("C-x j" . skk-mode)
+  :preface
+  (defun get-all-files-in-directory (directory)
+    "Get a list of all files in DIRECTORY."
+    (when (file-directory-p directory)
+      (directory-files-recursively directory "SKK-JISYO\\.[a-z]+" nil t)))
+  :config
+  (setq skk-extra-jisyo-file-list (get-all-files-in-directory "~/.emacs.d/etc/skk/")) ;;追加辞書
+  :custom
+  (default-input-method . "japanese-skk")
+  (skk-kutouten-type . 'jp) ;句読点には「、。」を利用する (デフォルト挙動だが明記する)
+  (skk-large-jisho . "~/.emacs.d/etc/skk/SKK-JISYO.L") ;Large辞書パス
+  (skk-jisyo . "~/.emacs.d/cache/skk/.skk-jisyo") ;個人用辞書パス
+  )
