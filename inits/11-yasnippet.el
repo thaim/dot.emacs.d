132876818f89d8f6742ada06e7903ae677134420
thaim <thaim24@gmail.com>
2026-06-03 23:27:34 +0900

    chore: disable yasnippet configuration
    yasnippet の leaf 設定をコメントアウトして無効化する。

--- inits/11-yasnippet.el
+++ inits/11-yasnippet.el
@@ -1,14 +1,15 @@
 ;;; yasnippet
-(leaf yasnippet
-  :ensure t
-  :config
-  (setq yas-snippet-dirs (append yas-snippet-dirs
-                                 (list
-                                  (locate-user-emacs-file "etc/yasnippets")
-                                  (locate-user-emacs-file "etc/yasnippet-snippets/snippets"))))
-  (custom-set-variables '(yas-trigger-key "TAB"))
-  (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet) ;; 既存スニペットを挿入する
-  (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet) ;; 新規スニペットを作成するバッファを用意する
-  (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file) ;; 既存スニペットを閲覧・編集する
-  (yas-global-mode 1)
-  )
+;; (leaf yasnippet
+;;   :ensure t
+;;   :config
+;;   (setq yas-snippet-dirs (append yas-snippet-dirs
+;;                                  (list
+;;                                   (locate-user-emacs-file "etc/yasnippets")
+;;                                   (locate-user-emacs-file "etc/yasnippet-snippets/snippets"))))
+;;   (custom-set-variables '(yas-trigger-key "TAB"))
+;;   (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet) ;; 既存スニペットを挿入する
+;;   (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet) ;; 新規スニペットを作成するバッファを用意する
+;;   (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file) ;; 既存スニペットを閲覧・編集する
+;;   (yas-global-mode 1)
+;;   )
+
