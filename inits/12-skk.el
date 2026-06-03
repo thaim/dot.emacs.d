b20a21d1c66ebfc407ba5b04ea0877821729fe12
thaim <thaim24@gmail.com>
2026-06-03 23:27:19 +0900

    refactor: restructure ddskk leaf config
    :bind を括弧で囲む記法に統一し、:custom の設定を :config 内の setq に移行する。

--- inits/12-skk.el
+++ inits/12-skk.el
@@ -5,8 +5,8 @@
 (leaf ddskk
   :ensure t
   :bind
-  ("C-x C-j" . skk-mode)
-  ("C-x j" . skk-mode)
+  (("C-x C-j" . skk-mode)
+   ("C-x j" . skk-mode))
   :preface
   (defun get-all-files-in-directory (directory)
     "Get a list of all files in DIRECTORY."
@@ -14,9 +14,8 @@
       (directory-files-recursively directory "SKK-JISYO\\.[a-z]+" nil t)))
   :config
   (setq skk-extra-jisyo-file-list (get-all-files-in-directory "~/.emacs.d/etc/skk/")) ;;追加辞書
-  :custom
-  (default-input-method . "japanese-skk")
-  (skk-kutouten-type . 'jp) ;句読点には「、。」を利用する (デフォルト挙動だが明記する)
-  (skk-large-jisho . "~/.emacs.d/etc/skk/SKK-JISYO.L") ;Large辞書パス
-  (skk-jisyo . "~/.emacs.d/cache/skk/.skk-jisyo") ;個人用辞書パス
+  (setq default-input-method "japanese-skk")
+  (setq skk-kutouten-type 'jp) ;句読点には「、。」を利用する (デフォルト挙動だが明記する)
+  (setq skk-large-jisho "~/.emacs.d/etc/skk/SKK-JISYO.L") ;Large辞書パス
+  (setq skk-jisyo "~/.emacs.d/cache/skk/.skk-jisyo") ;個人用辞書パス
   )
